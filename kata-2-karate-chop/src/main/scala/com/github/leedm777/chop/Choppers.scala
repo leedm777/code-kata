4b415cf95aa1aadef0ffe09dcd2f44b3c1f8b672
David M. Lee <david.m.lee@emerson.com>
2010-07-07 10:54:06 -0500

    Two choppers.

---
+++ kata-2-karate-chop/src/main/scala/com/github/leedm777/chop/Choppers.scala
@@ -0,0 +1,26 @@
+package com.github.leedm777.chop
+
+import java.util.Arrays
+
+object Choppers {
+    def fullRecursive(needle: Int, haystack: Array[Int]): Int = {
+        def f(n: Int): Int =
+            if (n >= haystack.size) -1
+            else if (haystack(n) == needle) n
+            else f(n + 1);
+        f(0);
+    }
+
+    def binaryRecursive(needle: Int, haystack: Array[Int]): Int = {
+        def f(begin: Int, end: Int): Int = {
+            if (begin == (end - 1)) {
+                if (needle == haystack(begin)) begin else -1
+            } else {
+                val mid = (begin + end) / 2
+                val midVal = haystack(mid)
+                if (needle < midVal) f(begin, mid) else f(mid, end)
+            }
+        }
+        if (haystack.isEmpty) -1 else f(0, haystack.size)
+    }
+}
---
+++ kata-2-karate-chop/src/test/scala/com/github/leedm777/chop/ChopTest.scala
@@ -0,0 +1,42 @@
+package com.github.leedm777.chop
+
+import org.scalatest.Spec
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class ChopTest extends Spec with ShouldMatchers {
+    describe("Chopper") {
+        describe("(full recursive)") {
+            it("should work") {shouldWork(Choppers.fullRecursive); }
+        }
+        describe("(binary recursive)") {
+            it("should work") {shouldWork(Choppers.binaryRecursive); }
+        }
+    }
+
+    def shouldWork(chop: (Int, Array[Int]) => Int) {
+        chop(3, Array()) should be(-1);
+        chop(3, Array(1)) should be(-1);
+        chop(1, Array(1)) should be(0);
+
+        chop(1, Array(1, 3, 5)) should be(0);
+        chop(3, Array(1, 3, 5)) should be(1);
+        chop(5, Array(1, 3, 5)) should be(2);
+        chop(0, Array(1, 3, 5)) should be(-1);
+        chop(2, Array(1, 3, 5)) should be(-1);
+        chop(4, Array(1, 3, 5)) should be(-1);
+        chop(6, Array(1, 3, 5)) should be(-1);
+
+        chop(1, Array(1, 3, 5, 7)) should be(0);
+        chop(3, Array(1, 3, 5, 7)) should be(1);
+        chop(5, Array(1, 3, 5, 7)) should be(2);
+        chop(7, Array(1, 3, 5, 7)) should be(3);
+        chop(0, Array(1, 3, 5, 7)) should be(-1);
+        chop(2, Array(1, 3, 5, 7)) should be(-1);
+        chop(4, Array(1, 3, 5, 7)) should be(-1);
+        chop(6, Array(1, 3, 5, 7)) should be(-1);
+        chop(8, Array(1, 3, 5, 7)) should be(-1);
+    }
+}
